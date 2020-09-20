58ef183da823bf526905570b6928cf34e6552e03
melaniede <debarrosmelanie@gmail.com>
2020-09-20 18:19:16 -0400

    Add sql files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");	
+
+INSERT INTO burgers (burger_name) VALUES ("Onion Burger");	
+
+
+
