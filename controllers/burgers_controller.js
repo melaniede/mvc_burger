20e0a0c9014ce2b404b28caffc5d925984f21c72
melaniede <debarrosmelanie@gmail.com>
2020-10-20 20:59:50 -0400

    update burger.js link

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // Import the model (burgers.js) to use its database functions
-const burger = require("../models/burgers.js");
+const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required
 router.get("/", async function(req, res) {
